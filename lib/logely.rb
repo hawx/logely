3fcb77427bc6d74992f8133ed6b62d91cfd92f2a
Joshua Hawxwell <m@hawx.me>
2013-07-13 17:19:31 +0100

    Removed clive/output dependency

--- lib/logely.rb
+++ lib/logely.rb
@@ -1,6 +1,102 @@
-require 'clive/output'
+class String
+
+  # @example
+  #
+  #   puts "bold".bold
+  #   puts "underline".underline
+  #   puts "blink".blink
+  #   puts "green".green
+  #   puts "red".red
+  #   puts "magenta".magenta
+  #   puts "yellow".yellow
+  #   puts "blue".blue
+  #   puts "grey".grey
+  #
+  #   puts "combo".blue.bold.underline.blink
+  #
+  # @param code
+  #   Colour code, see http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+  #
+  def colour(code)
+    r = "\e[#{code}m#{self}"
+    r << "\e[0m" unless self[-4..-1] == "\e[0m"
+    r
+  end
+
+  # Remove any colour codes from a string.
+  def clear_colours
+    gsub /\e\[?\d\d{0,2}m/, ''
+  end
+
+  COLOURS = {
+    "black"   => 0,
+    "red"     => 1,
+    "green"   => 2,
+    "yellow"  => 3,
+    "blue"    => 4,
+    "magenta" => 5,
+    "cyan"    => 6,
+    "white"   => 7
+  }.each do |name, code|
+    define_method name do
+      colour "3#{code}"
+    end
+
+    define_method "#{name}!" do
+      colour! "3#{code}"
+    end
+
+    define_method "#{name}_bg" do
+      colour "4#{code}"
+    end
+
+    define_method "#{name}_bg!" do
+      colour! "4#{code}"
+    end
+
+    # Change name to grey instead of l_black
+    l_name = "l_#{name}"
+    if name == "black"
+      l_name = "grey"
+    end
+
+    define_method "#{l_name}" do
+      colour "9#{code}"
+    end
+
+    define_method "#{l_name}!" do
+      colour! "9#{code}"
+    end
+
+    define_method "#{l_name}_bg" do
+      colour "10#{code}"
+    end
+
+    define_method "#{l_name}_bg!" do
+      colour! "10#{code}"
+    end
+  end
+
+  {
+    "bold"      => 1,
+    "underline" => 4,
+    "blink"     => 5,
+    "reverse"   => 7
+  }.each do |name, code|
+    define_method name do
+      colour code
+    end
+
+    define_method "#{name}!" do
+      colour! code
+    end
+  end
+
+end
 
 class Logely
+
+  VERSION = '0.0.0'
 
   DEFAULT = {
     padding: 2,
--- logely.gemspec
+++ logely.gemspec
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-require File.expand_path("../lib/logely/version", __FILE__)
+require File.expand_path("../lib/logely", __FILE__)
 
 Gem::Specification.new do |s|
   s.name         = "logely"
@@ -14,7 +14,7 @@ Gem::Specification.new do |s|
     in a properly spaced left column.
   DESC
 
-  s.files        = %w(README.md Rakefile LICENCE)
+  s.files        = %w(README.md Rakefile)
   s.files       += Dir["{lib,spec}/**/*"] & `git ls-files`.split("\n")
   s.test_files   = Dir["spec/**/*"] & `git ls-files`.split("\n")
 end
